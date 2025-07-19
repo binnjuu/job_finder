6f707beefafdbbc7f85bb782d436117643f43e40
binnjuu <109500598+binnjuu@users.noreply.github.com>
2025-07-20 06:46:39 +0800

    調整104網站資料抓取方式

--- site_104.py
+++ site_104.py
@@ -2,6 +2,10 @@ import json
 import settings
 import time
 from _self_packages.chrome_driver import ChromeDriver
+from _self_packages import send
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 
 
 url = r"https://www.104.com.tw/jobs/search/?area=6001001000&jobsource=joblist_search&mode=s&page=1"
@@ -19,14 +23,36 @@ driver = chrome_driver.monitor()
 driver.get(url)
 time.sleep(3)
 
-log = chrome_driver.request_log()
-for obj in log:
-    # print(obj["請求網址"])
-    if "https://www.104.com.tw/jobs/search/api/jobs" in obj["請求網址"]:
-        print(obj["請求網址"])
-        result = json.loads(obj["回傳內容"]["body"])
+item_eles = WebDriverWait(driver, 10).until(
+    EC.presence_of_all_elements_located((By.CSS_SELECTOR, f'div.recycle-scroller--item'))
+)
 
-jobs = result["data"]
-print(jobs)
-print(type(jobs))
-print(len(jobs))
+# 資料擷取
+for item in item_eles:
+    print("-------")
+    link = item.find_element(By.CSS_SELECTOR, "div.info-container div.info-job > h2 > a").get_attribute("href")
+    title = item.find_element(By.CSS_SELECTOR, "div.info-container div.info-job > h2").text
+    company = item.find_element(By.CSS_SELECTOR, "div.info-container div.info-company > a").text
+    job_content = item.find_element(By.CSS_SELECTOR, "div.info-container div.info-description").text
+    
+    other_info = item.find_elements(By.CSS_SELECTOR, "div.info-container div.info-tags > span.info-tags__text")
+    area = other_info[0].text
+    experience = other_info[1].text
+    education = other_info[2].text
+    salary = other_info[3].text
+
+    update = item.find_element(By.CSS_SELECTOR, "div.date-container").text
+
+    info = {
+        "連結": link,
+        "標題": title,
+        "企業": company,
+        "地區": area,
+        "薪資": salary,
+        "學歷": education,
+        "經驗": experience,
+        "工作內容": job_content,
+        "更新": update,
+    }
+    
+    send.pretty(info)
