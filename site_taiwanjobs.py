f3331473d2378f2ca3a942a423612e6b554ed3bb
binnjuu <109500598+binnjuu@users.noreply.github.com>
2025-07-20 16:49:43 +0800

    taiwanjobs程式碼整理

--- site_taiwanjobs.py
+++ site_taiwanjobs.py
@@ -6,57 +6,54 @@ from _self_packages import send
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+
 
 url = r"https://job.taiwanjobs.gov.tw/Internet/Index/job_search_list.aspx?qStamp=638885484970095899"
 
-#監聽Chrome Driver
-chrome_driver = ChromeDriver(port=settings.port, driver_path=settings.driver_path, profile_save_path=settings.profile_save_path)
-while True:
-    if not chrome_driver.check_port():
-        chrome_driver.launch(account=settings.account)
-    else:
-        break
+def search(driver:webdriver.Chrome, url:str) -> list:
+    """
+    進入指令網址抓取工作資料，整理後以list格式回傳。
+    """
+    driver.get(url)
 
-driver = chrome_driver.monitor()
+    item_eles = WebDriverWait(driver, 10).until(
+        EC.presence_of_all_elements_located((By.CSS_SELECTOR, f'ul.search-list-items li.search-list-item'))
+    )
 
-driver.get(url)
-time.sleep(3)
+    # 資料擷取
+    jobs_list = []
+    for item in item_eles:
+        # 職缺ID
+        hire_id = item.get_attribute("id").replace("hire_", "")
 
-item_eles = WebDriverWait(driver, 10).until(
-    EC.presence_of_all_elements_located((By.CSS_SELECTOR, f'ul.search-list-items li.search-list-item'))
-)
+        # 企業ID
+        employer_href = item.find_element(By.CSS_SELECTOR, "a.t-card-comp-name").get_attribute("href")
+        target_idx = employer_href.index("EMPLOYER_ID=")
+        employer_id = employer_href[target_idx : employer_href.index("&", target_idx)].replace("EMPLOYER_ID=", "")
 
-# 資料擷取
-for item in item_eles:
-    print("-------")
-    # 職缺ID
-    hire_id = item.get_attribute("id").replace("hire_", "")
+        # 其他資訊
+        title = item.find_element(By.CSS_SELECTOR, "div a.text-inherit").text
+        company = item.find_element(By.CSS_SELECTOR, "div a.t-card-comp-name").text
+        job_content = item.find_element(By.CSS_SELECTOR, "div.t-list-substance").text
+        area = item.find_element(By.CSS_SELECTOR, "li.t-card-area").text
+        salary = item.find_element(By.CSS_SELECTOR, "li.t-list-txts-item text.t-money").text
+        education = item.find_element(By.CSS_SELECTOR, "li.t-card-edu").text
+        experience = item.find_element(By.CSS_SELECTOR, "li.t-card-workexp").text
+        update = item.find_element(By.CSS_SELECTOR, "text.t-list-date").text
 
-    # 企業ID
-    employer_href = item.find_element(By.CSS_SELECTOR, "a.t-card-comp-name").get_attribute("href")
-    target_idx = employer_href.index("EMPLOYER_ID=")
-    employer_id = employer_href[target_idx : employer_href.index("&", target_idx)].replace("EMPLOYER_ID=", "")
-
-    # 其他資訊
-    title = item.find_element(By.CSS_SELECTOR, "div a.text-inherit").text
-    company = item.find_element(By.CSS_SELECTOR, "div a.t-card-comp-name").text
-    job_content = item.find_element(By.CSS_SELECTOR, "div.t-list-substance").text
-    area = item.find_element(By.CSS_SELECTOR, "li.t-card-area").text
-    salary = item.find_element(By.CSS_SELECTOR, "li.t-list-txts-item text.t-money").text
-    education = item.find_element(By.CSS_SELECTOR, "li.t-card-edu").text
-    experience = item.find_element(By.CSS_SELECTOR, "li.t-card-workexp").text
-    update = item.find_element(By.CSS_SELECTOR, "text.t-list-date").text
-
-    info = {
-        "連結": f"https://job.taiwanjobs.gov.tw/Internet/Index/JobDetail.aspx?EMPLOYER_ID={employer_id}&HIRE_ID={hire_id}",
-        "標題": title,
-        "企業": company,
-        "地區": area,
-        "薪資": salary,
-        "學歷": education,
-        "經驗": experience,
-        "工作內容": job_content,
-        "更新": update,
-    }
+        job = {
+            "連結": f"https://job.taiwanjobs.gov.tw/Internet/Index/JobDetail.aspx?EMPLOYER_ID={employer_id}&HIRE_ID={hire_id}",
+            "標題": title,
+            "企業": company,
+            "地區": area,
+            "薪資": salary,
+            "學歷": education,
+            "經驗": experience,
+            "工作內容": job_content,
+            "更新": update,
+        }
+        
+        jobs_list.append(job)
     
-    send.pretty(info)
+    return jobs_list
