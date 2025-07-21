6932bcb6f615db23c8fdb57f9ba425f4b5ce6342
binnjuu <109500598+binnjuu@users.noreply.github.com>
2025-07-22 03:18:26 +0800

    新增data_check.py，並建立判斷資料是否以儲存在json中的功能

---
+++ data_check.py
@@ -0,0 +1,22 @@
+from today_json import Today_Json
+
+
+def json_file(job:dict):
+    """
+    檢查某個職缺資料的連結是否已經在輸出的json檔案中
+    """
+    today_json = Today_Json()
+    json_contents = today_json.read()
+    if json_contents == -1:
+        return False
+    
+    all_links = []
+    for content in json_contents:
+        all_links.append(content["連結"])
+
+    if job["連結"] in all_links:
+        print("存在")
+        return True
+    else:
+        print("不存在")
+        return False
