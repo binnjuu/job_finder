7173c160a7bb5bbb26616e3df54d12a6a27cbeb7
binnjuu <109500598+binnjuu@users.noreply.github.com>
2025-07-22 23:05:25 +0800

    調整為class

--- site_1111.py
+++ site_1111.py
@@ -2,52 +2,53 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
-import url_revision
 
 
-def search(driver:webdriver.Chrome, url:str, page=1) -> list:
+class Site_1111():
     """
-    進入1111網址抓取工作資料，整理後以list格式回傳。
+    在指定的1111網址中抓取職缺資料
     """
-    # 修改網址中的頁碼
-    url = url_revision.page_number(url=url, page=page)
-    
-    driver.get(url=url)
+    def __init__(self, driver:webdriver.Chrome):
+        self.driver = driver
+        
+    def scraping(self):
+        """
+        抓取目前的頁面資料整理後，以list格式回傳
+        """
+        item_eles = WebDriverWait(self.driver, timeout=10, poll_frequency=0.5).until(
+            EC.presence_of_all_elements_located((By.CSS_SELECTOR, f'div.search-content div.job-card'))
+        )
 
-    item_eles = WebDriverWait(driver, timeout=10, poll_frequency=0.5).until(
-        EC.presence_of_all_elements_located((By.CSS_SELECTOR, f'div.search-content div.job-card'))
-    )
+        # 資料擷取
+        jobs_list = []
+        for item in item_eles:
+            id = item.get_attribute("data-purpose")
+            all_h2_eles = item.find_elements(By.CSS_SELECTOR, "h2")
+            title = all_h2_eles[0].text
+            company = all_h2_eles[1].text
+            job_content = item.find_elements(By.CSS_SELECTOR, "p")[1].text
 
-    # 資料擷取
-    jobs_list = []
-    for item in item_eles:
-        id = item.get_attribute("data-purpose")
-        all_h2_eles = item.find_elements(By.CSS_SELECTOR, "h2")
-        title = all_h2_eles[0].text
-        company = all_h2_eles[1].text
-        job_content = item.find_elements(By.CSS_SELECTOR, "p")[1].text
+            other_info = item.find_elements(By.CSS_SELECTOR, ".job-card-condition .job-card-condition__text")
+            area = other_info[0].text
+            salary = other_info[1].text
+            education = other_info[2].text
+            experience = other_info[3].text
 
-        other_info = item.find_elements(By.CSS_SELECTOR, ".job-card-condition .job-card-condition__text")
-        area = other_info[0].text
-        salary = other_info[1].text
-        education = other_info[2].text
-        experience = other_info[3].text
+            update = item.find_element(By.CSS_SELECTOR, "div.job-summary").text
+            update = update.replace(" ", "")[:5] # 刪除應徵人數資訊
 
-        update = item.find_element(By.CSS_SELECTOR, "div.job-summary").text
-        update = update.replace(" ", "")[:5] # 刪除應徵人數資訊
+            job = {
+                "連結": f"https://www.1111.com.tw/job/{id}",
+                "標題": title,
+                "企業": company,
+                "地區": area,
+                "薪資": salary,
+                "學歷": education,
+                "經驗": experience,
+                "說明": job_content,
+                "更新": update,
+            }
+            jobs_list.append(job)
 
-        job = {
-            "連結": f"https://www.1111.com.tw/job/{id}",
-            "標題": title,
-            "企業": company,
-            "地區": area,
-            "薪資": salary,
-            "學歷": education,
-            "經驗": experience,
-            "說明": job_content,
-            "更新": update,
-        }
-        jobs_list.append(job)
-
-    return jobs_list
+        return jobs_list
         
--- site_taiwanjobs.py
+++ site_taiwanjobs.py
@@ -4,49 +4,54 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
 
 
-def search(driver:webdriver.Chrome, url:str) -> list:
+class Site_Taiwanjobs():
     """
-    進入台灣就業通網址抓取工作資料，整理後以list格式回傳。
+    在指定的1111網址中抓取職缺資料
     """
-    driver.get(url)
-
-    item_eles = WebDriverWait(driver, timeout=10, poll_frequency=0.5).until(
-        EC.presence_of_all_elements_located((By.CSS_SELECTOR, f'ul.search-list-items li.search-list-item'))
-    )
-
-    # 資料擷取
-    jobs_list = []
-    for item in item_eles:
-        # 職缺ID
-        hire_id = item.get_attribute("id").replace("hire_", "")
-
-        # 企業ID
-        employer_href = item.find_element(By.CSS_SELECTOR, "a.t-card-comp-name").get_attribute("href")
-        target_idx = employer_href.index("EMPLOYER_ID=")
-        employer_id = employer_href[target_idx : employer_href.index("&", target_idx)].replace("EMPLOYER_ID=", "")
-
-        # 其他資訊
-        title = item.find_element(By.CSS_SELECTOR, "div a.text-inherit").text
-        company = item.find_element(By.CSS_SELECTOR, "div a.t-card-comp-name").text
-        job_content = item.find_element(By.CSS_SELECTOR, "div.t-list-substance").text
-        area = item.find_element(By.CSS_SELECTOR, "li.t-card-area").text
-        salary = item.find_element(By.CSS_SELECTOR, "li.t-list-txts-item text.t-money").text
-        education = item.find_element(By.CSS_SELECTOR, "li.t-card-edu").text
-        experience = item.find_element(By.CSS_SELECTOR, "li.t-card-workexp").text
-        update = item.find_element(By.CSS_SELECTOR, "text.t-list-date").text
-
-        job = {
-            "連結": f"https://job.taiwanjobs.gov.tw/Internet/Index/JobDetail.aspx?EMPLOYER_ID={employer_id}&HIRE_ID={hire_id}",
-            "標題": title,
-            "企業": company,
-            "地區": area,
-            "薪資": salary,
-            "學歷": education,
-            "經驗": experience,
-            "說明": job_content,
-            "更新": update,
-        }
+    def __init__(self, driver:webdriver.Chrome):
+        self.driver = driver
         
-        jobs_list.append(job)
-    
-    return jobs_list
+    def scraping(self):
+        """
+        抓取目前的頁面資料整理後，以list格式回傳
+        """
+        item_eles = WebDriverWait(self.driver, timeout=10, poll_frequency=0.5).until(
+            EC.presence_of_all_elements_located((By.CSS_SELECTOR, f'ul.search-list-items li.search-list-item'))
+        )
+
+        # 資料擷取
+        jobs_list = []
+        for item in item_eles:
+            # 職缺ID
+            hire_id = item.get_attribute("id").replace("hire_", "")
+
+            # 企業ID
+            employer_href = item.find_element(By.CSS_SELECTOR, "a.t-card-comp-name").get_attribute("href")
+            target_idx = employer_href.index("EMPLOYER_ID=")
+            employer_id = employer_href[target_idx : employer_href.index("&", target_idx)].replace("EMPLOYER_ID=", "")
+
+            # 其他資訊
+            title = item.find_element(By.CSS_SELECTOR, "div a.text-inherit").text
+            company = item.find_element(By.CSS_SELECTOR, "div a.t-card-comp-name").text
+            job_content = item.find_element(By.CSS_SELECTOR, "div.t-list-substance").text
+            area = item.find_element(By.CSS_SELECTOR, "li.t-card-area").text
+            salary = item.find_element(By.CSS_SELECTOR, "li.t-list-txts-item text.t-money").text
+            education = item.find_element(By.CSS_SELECTOR, "li.t-card-edu").text
+            experience = item.find_element(By.CSS_SELECTOR, "li.t-card-workexp").text
+            update = item.find_element(By.CSS_SELECTOR, "text.t-list-date").text
+
+            job = {
+                "連結": f"https://job.taiwanjobs.gov.tw/Internet/Index/JobDetail.aspx?EMPLOYER_ID={employer_id}&HIRE_ID={hire_id}",
+                "標題": title,
+                "企業": company,
+                "地區": area,
+                "薪資": salary,
+                "學歷": education,
+                "經驗": experience,
+                "說明": job_content,
+                "更新": update,
+            }
+            
+            jobs_list.append(job)
+        
+        return jobs_list
