2acfedc4f7937e8d65ef90a9ed7b9df544e79384
binnjuu <109500598+binnjuu@users.noreply.github.com>
2025-07-23 02:32:34 +0800

    目前可讀取三個網站的資料後送出訊息

--- main.py
+++ main.py
@@ -38,51 +38,76 @@ site_taiwanjobs = Site_Taiwanjobs(driver=driver)
 
 date = str(datetime.today().date())[-5:].replace("-", "/") # 取得今天的日期，格式: 07/21
 
-page_number = 1
-jobs_list = []
-while page_number <= MAX_PAGE_LIMIT:
-    print(f"正在載入第{page_number}頁")
-    load_page(url=settings.url["104"], page=page_number)
-    jobs_list += site_104.scraping()
+def scraping_data(site_key):
+    page_number = 1
+    jobs_list = []
+    while page_number <= MAX_PAGE_LIMIT:
+        print(f"正在讀取{site_key}網站第{page_number}頁...")
+        if site_key == "taiwanjobs":
+            driver.get(settings.url[site_key])
+        else:
+            load_page(url=settings.url[site_key], page=page_number)
+        jobs_list += site_list[site_key]()
 
-    # 如果資料抓取失敗或是沒有資料則中斷
-    if len(jobs_list) <= 0:
-        print("資料抓取失敗或是沒有資料")
-        break
-    # 如果最後一筆資料不是當天的則中斷
-    elif jobs_list[-1]["更新"] != date:
-        print("最後一筆資料不是當天的")
-        break
-    # 如果最後一筆資料已經儲存在json檔案中則中斷
-    elif data_check.json_file(jobs_list[-1]):
-        print("最後一筆資料已經儲存在json檔案")
-        break
+        # 如果資料抓取失敗或是沒有資料則中斷
+        if len(jobs_list) <= 0:
+            print("資料抓取失敗或是沒有資料")
+            break
+        # 如果最後一筆資料不是當天的則中斷
+        elif jobs_list[-1]["更新"] != date:
+            print("最後一筆資料不是當天的")
+            break
+        # 如果最後一筆資料已經儲存在json檔案中則中斷
+        elif data_check.json_file(jobs_list[-1]):
+            print("最後一筆資料已經儲存在json檔案")
+            break
 
-    # 否則繼續抓取下一頁
-    page_number += 1
-    time.sleep(3) # 避免載入頻率過高
+        # 否則繼續抓取下一頁
+        page_number += 1
+        time.sleep(3) # 避免載入頻率過高
+    
+    return jobs_list
 
-print(f"共找到: {len(jobs_list)}筆資料")
+
+site_list = {
+    "104": site_104.scraping,
+    "1111": site_1111.scraping,
+    "taiwanjobs": site_taiwanjobs.scraping,
+}
+
+all_jobs = {}
+for key in site_list.keys():
+    all_jobs[key] = scraping_data(key)
+    print(f"在{key}共找到{len(all_jobs[key])}筆資料")
+
 
 # 篩選出還未儲存的當日職缺
-filter_jobs_list = []
-for job in jobs_list:
-    if jobs_list[-1]["更新"] != date:
-        continue
-    elif not data_check.json_file(jobs_list[-1]):
-        filter_jobs_list.append(job)
+all_filter_jobs = {}
+for key in all_jobs.keys():
+    jobs_list = all_jobs[key]
+    for job in jobs_list:
+        if job["更新"] != date:
+            continue
+        elif not data_check.json_file(job):
+            if key not in all_filter_jobs.keys():
+                all_filter_jobs[key] = []
+            all_filter_jobs[key].append(job)
 
 # 整理後送出discord訊息
 bot_message_list = []
-for job in filter_jobs_list:
-    bot_message = f"\n### {job["更新"]} | [{job["標題"]}]({job["連結"]})\n"
-    bot_massage += f"```[企業] {job["企業"]}\n"
-    bot_massage += f"[地區] {job["地區"]}\n"
-    bot_massage += f"[學歷] {job["學歷"]}\n"
-    bot_massage += f"[經驗] {job["經驗"]}\n"
-    bot_massage += f"[薪資] {job["薪資"]}\n"
-    bot_massage += f"[說明]\n{job["說明"]}```"
-    bot_message_list.append(bot_massage)
-kawaii_fox.start(api_key=settings.discord_bot_key, channel_id=settings.channel_id, message=bot_message_list)
+for filter_jobs in all_filter_jobs.values():
+    for job in filter_jobs:
+        bot_message = f"\n### {job["更新"]} | [{job["標題"]}](<{job["連結"]}>)\n"
+        bot_message += f"```[企業] {job["企業"]}\n"
+        bot_message += f"[地區] {job["地區"]}\n"
+        bot_message += f"[學歷] {job["學歷"]}\n"
+        bot_message += f"[經驗] {job["經驗"]}\n"
+        bot_message += f"[薪資] {job["薪資"]}\n"
+        bot_message += f"[說明]\n{job["說明"]}```"
+        bot_message_list.append(bot_message)
+if len(bot_message_list) > 0:
+    print(f"共篩選出{len(bot_message_list)}個職缺資訊")
+    kawaii_fox.start(api_key=settings.discord_bot_key, channel_id=settings.channel_id, message=bot_message_list)
 
-# today_json.write(content=new_jobs_104)
+# 每次抓完資料都會替換json內容為最新的資料，再看要不要改成新增的
+today_json.write(content=all_jobs)
