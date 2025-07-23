2a09bb68bb545181559fb956a28391617ea11302
binnjuu <109500598+binnjuu@users.noreply.github.com>
2025-07-23 08:49:09 +0800

    整理主程式邏輯

--- main.py
+++ main.py
@@ -10,9 +10,8 @@ from site_104 import Site_104
 from site_1111 import Site_1111
 from site_taiwanjobs import Site_Taiwanjobs
 import data_check
-import url_revision
 
-MAX_PAGE_LIMIT = 1
+MAX_PAGE_LIMIT = 2
 
 #監聽Chrome Driver
 chrome_driver = ChromeDriver(port=settings.port, driver_path=settings.driver_path, profile_save_path=settings.profile_save_path)
@@ -24,61 +23,65 @@ while True:
 
 driver = chrome_driver.monitor()
 
-def load_page(url:str, page=1):
-    """
-    載入指定的網址，可利用page=修改頁碼
-    """
-    url = url_revision.page_number(url=url, page=page) # 修改網址中的頁碼
-    driver.get(url)
-
 today_json = Today_Json()
-site_104 = Site_104(driver=driver)
-site_1111 = Site_1111(driver=driver)
-site_taiwanjobs = Site_Taiwanjobs(driver=driver)
+site_104 = Site_104(driver=driver, url=settings.url["104"])
+site_1111 = Site_1111(driver=driver, url=settings.url["1111"])
+site_taiwanjobs = Site_Taiwanjobs(driver=driver, url=settings.url["taiwanjobs"])
 
 date = str(datetime.today().date())[-5:].replace("-", "/") # 取得今天的日期，格式: 07/21
 
-def scraping_data(site_key):
-    page_number = 1
-    jobs_list = []
-    while page_number <= MAX_PAGE_LIMIT:
-        print(f"正在讀取{site_key}網站第{page_number}頁...")
-        if site_key == "taiwanjobs":
-            driver.get(settings.url[site_key])
-        else:
-            load_page(url=settings.url[site_key], page=page_number)
-        jobs_list += site_list[site_key]()
-
-        # 如果資料抓取失敗或是沒有資料則中斷
-        if len(jobs_list) <= 0:
-            print("資料抓取失敗或是沒有資料")
-            break
-        # 如果最後一筆資料不是當天的則中斷
-        elif jobs_list[-1]["更新"] != date:
-            print("最後一筆資料不是當天的")
-            break
-        # 如果最後一筆資料已經儲存在json檔案中則中斷
-        elif data_check.json_file(jobs_list[-1]):
-            print("最後一筆資料已經儲存在json檔案")
-            break
-
-        # 否則繼續抓取下一頁
-        page_number += 1
-        time.sleep(3) # 避免載入頻率過高
-    
-    return jobs_list
-
-
-site_list = {
-    "104": site_104.scraping,
-    "1111": site_1111.scraping,
-    "taiwanjobs": site_taiwanjobs.scraping,
-}
-
 all_jobs = {}
-for key in site_list.keys():
-    all_jobs[key] = scraping_data(key)
-    print(f"在{key}共找到{len(all_jobs[key])}筆資料")
+
+# 1111
+jobs_1111 = []
+for times in range(MAX_PAGE_LIMIT):
+    if times == 0:
+        site_1111.load_page()
+    else:
+        site_1111.next_page()
+    jobs_list = site_1111.scraping()
+    jobs_1111 += jobs_list
+
+    # 檢查要不要載入下一頁
+    if not data_check.next_page(jobs_list=jobs_list, date=date):
+        break
+    time.sleep(3) # 避免載入頻率過高
+all_jobs["1111"] = jobs_1111
+send.message(f"找到{len(jobs_1111)}個")
+
+# 104
+jobs_104 = []
+for times in range(MAX_PAGE_LIMIT):
+    if times == 0:
+        site_104.load_page()
+    else:
+        site_104.next_page()
+    jobs_list = site_104.scraping()
+    jobs_104 += jobs_list
+
+    # 檢查要不要載入下一頁
+    if not data_check.next_page(jobs_list=jobs_list, date=date):
+        break
+    time.sleep(3) # 避免載入頻率過高
+all_jobs["104"] = jobs_104
+send.message(f"找到{len(jobs_104)}個")
+
+# taiwanjobs
+jobs_taiwanjobs = []
+for times in range(MAX_PAGE_LIMIT):
+    if times == 0:
+        site_taiwanjobs.load_page()
+    else:
+        site_taiwanjobs.next_page()
+    jobs_list = site_taiwanjobs.scraping()
+    jobs_taiwanjobs = jobs_list # 台灣就業通的所有頁數資料都會在同一個頁面上，所以每次都覆蓋掉舊的資料
+
+    #檢查要不要載入下一頁
+    if not data_check.next_page(jobs_list=jobs_list, date=date):
+        break
+    time.sleep(3) # 避免載入頻率過高
+all_jobs["taiwanjobs"] = jobs_taiwanjobs
+send.message(f"找到{len(jobs_taiwanjobs)}個")
 
 
 # 篩選出還未儲存的當日職缺
