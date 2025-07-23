c6395e77589c5c21325c21f95438ea222a13389f
binnjuu <109500598+binnjuu@users.noreply.github.com>
2025-07-24 06:20:48 +0800

    _self_packages資料夾改名為self_packages

--- self_packages/chrome_driver.py
+++ self_packages/chrome_driver.py
@@ -3,7 +3,7 @@ from selenium.webdriver.chrome.service import Service
 import os
 import socket
 import json
-from _self_packages import send
+from self_packages import send
 
 class ChromeDriver:
     """
