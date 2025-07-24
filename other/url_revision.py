9290d85bd028cd5c9bf533d8386d0ab16864d4cf
binnjuu <109500598+binnjuu@users.noreply.github.com>
2025-07-24 11:44:02 +0800

    調整修改網址頁數的方式

--- other/url_revision.py
+++ other/url_revision.py
@@ -1,14 +1,15 @@
 import re
 
-def page_number(url:str, page=1):
+def page_number(url:str, arg:str, page=1):
     """
-    修改網址中的page=參數
+    arg=參數名稱
+    修改網址中的頁面參數
     """
     # 在網址中找目前的頁碼
-    currently_page_compile = re.compile(r"page=\d*")
-    currently_page = currently_page_compile.search(url).group().replace("page=", "")
+    currently_page_compile = re.compile(rf"{arg}\d*")
+    currently_page = currently_page_compile.search(url).group().replace(arg, "")
     
     # 將頁碼調整為指定值
-    revision_url = url.replace(f"page={currently_page}", f"page={page}")
+    revision_url = url.replace(f"{arg}{currently_page}", f"{arg}{page}")
     
     return revision_url
